754724072c5d7d2ba7e27417b9826269ffe48bd4
pdvladking <pdvladking@gmail.com>
2026-05-24 10:49:08 +0545

    added menu data + menupg

--- app/menu/page.js
+++ app/menu/page.js
@@ -1,8 +1,29 @@
+import dishes from '@/data/menuData';
+import Image from "next/image";
+
 export default function MenuPage() {
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold text-orange-600">Menu</h1>
-      <p>Here's where customers browse dishes</p>
+      <h1 className="text-4xl font-bold text-orange-600 mb-6">Menu</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {dishes.map((dish) => (
+          <div
+            key={dish.id}
+            className="bg-white shadow-md rounded-lg overflow-hidden hover:scale-105 transition-transform"
+          >
+            <Image src={dish.image}
+             alt={dish.name}
+              width={400}
+              height={250}
+              className="w-full h-40 object-cover"
+              />
+            <div className="p-4">
+              <h2 className="text-xl font-semibold">{dish.name}</h2>
+              <p className="text-gray-600">{dish.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ data/menuData.js
@@ -0,0 +1,7 @@
+const dishes = [
+  { id: 1, name: "Margherita Pizza", price: "NPR 450", image: "/pizza.jpg" },
+  { id: 2, name: "Margherita Pizza", price: "NPR 450", image: "/pizza.jpg" },
+  { id: 3, name: "Margherita Pizza", price: "NPR 450", image: "/pizza.jpg" },
+]
+
+export default dishes;
