03dd203ee040e65b916a4e8a6891aaf326edd7a3
pdvladking <pdvladking@gmail.com>
2026-05-24 10:06:04 +0545

    Initial Next.js 14 + Tailwind + App Router setup

---
+++ app/menu/page.js
@@ -0,0 +1,8 @@
+export default function MenuPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-orange-600">Menu</h1>
+      <p>Here's where customers browse dishes</p>
+    </div>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-4xl font-bold text-orange-600">Gastro Guide</h1>
+}
